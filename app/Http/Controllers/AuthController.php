9aefec9391b9ad78e791b5c338a064bd3daa3513
intakhabchy <intakhab.chy@gmail.com>
2025-10-01 01:01:40 +0600

    login works

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -24,6 +25,31 @@ class AuthController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'User created successfully'
+        ]);
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = User::where('email',$request->email)->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => true,
+            'token' => $token,
+            'message' => 'Login Successful'
         ]);
     }
 }
